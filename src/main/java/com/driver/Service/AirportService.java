c739ac8b512824fcfd3b97a78e3c22bad19371c3
Sahil Dhiman <dhiman.sahil03022001@gmail.com>
2023-06-07 22:11:52 +0530

    c8

--- src/main/java/com/driver/Service/AirportService.java
+++ src/main/java/com/driver/Service/AirportService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
-@Service
 public class AirportService {
 
     AirportRepository airportRepository = new AirportRepository();
